e3d48a4918eb55b4c0b3872910fd7fd439b96ea7
yil3 <1yif@sina.com>
2022-11-05 01:15:25 +0800

    ..

--- authorization/src/main.rs
+++ authorization/src/main.rs
@@ -1,16 +1,8 @@
 use authorization::router::router;
 use x_core::application::Application;
 
-use time::format_description::well_known::Rfc3339;
-
-pub fn test() {
-    let a = datetime!(2019-01-01).format(&Rfc3339);
-    println!("{}", a);
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    test();
     Application::serve(router()).run().await?;
     Ok(())
 }
