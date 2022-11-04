8c13716411b4bb29290dfc137db9049b6ad08463
yil3 <1yif@sina.com>
2022-11-04 23:40:04 +0800

    ..

--- authorization/src/dto/response/client_responses.rs
+++ authorization/src/dto/response/client_responses.rs
@@ -1,7 +1,30 @@
-use serde::{Deserialize, Serialize};
-use serde_with::serde_as;
+use serde::{Deserialize, Serialize, Deserializer, Serializer};
+use serde_with::{serde_as, DeserializeAs, SerializeAs};
 use time::OffsetDateTime;
 use time::format_description::well_known::Rfc3339;
+
+struct DateTimeFormat;
+
+impl SerializeAs<OffsetDateTime> for DateTimeFormat {
+    fn serialize_as<S>(date_time: &OffsetDateTime, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let format = time::format_description::parse("[year]-[month]-[day] [hour]:[minute]:[second]").unwrap();
+        serializer.serialize_str(&date_time.format(&format).map_err(serde::ser::Error::custom)?)
+    }
+}
+
+impl<'de> DeserializeAs<'de, OffsetDateTime> for DateTimeFormat {
+    fn deserialize_as<D>(deserializer: D) -> Result<OffsetDateTime, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let format = time::format_description::parse("[year]-[month]-[day] [hour]:[minute]:[second]").unwrap();
+        let s = String::deserialize(deserializer)?;
+        OffsetDateTime::parse(&s, &format).map_err(serde::de::Error::custom)
+    }
+}
 
 
 #[serde_as]
@@ -14,7 +37,7 @@ pub struct ClientResponse {
     pub redirect_uri: String,
     pub scope: String,
     pub owner: Option<String>,
-    #[serde_as(as = "Rfc3339")]
+    #[serde_as(as = "DateTimeFormat")]
     pub created_at: OffsetDateTime,
     #[serde_as(as = "Option<Rfc3339>")]
     pub updated_at: Option<OffsetDateTime>,
--- authorization/src/entity/client.rs
+++ authorization/src/entity/client.rs
@@ -2,12 +2,15 @@ use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 use sqlx::FromRow;
 use time::OffsetDateTime;
+use time::format_description::well_known::Rfc3339;
+
 /**
 * @Author xYx
 * @Date 2022-09-26 10:50:23
 */
-#[derive(FromRow, Deserialize, Serialize, Debug)]
+
 #[serde_as]
+#[derive(FromRow, Deserialize, Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ClientEntity {
     pub id: String,
@@ -18,6 +21,6 @@ pub struct ClientEntity {
     pub owner: Option<String>,
     #[serde_as(as = "Rfc3339")]
     pub created_at: OffsetDateTime,
-    #[serde_as(as = "Rfc3339")]
+    #[serde_as(as = "Option<Rfc3339>")]
     pub updated_at: Option<OffsetDateTime>,
 }
