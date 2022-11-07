74cb4360a1d22f8cc790ebe2fab6626225478803
xYx <1yif@sina.com>
2022-11-07 18:03:36 +0800

    ..

--- view/authorize-view/src/layouts/components/Header/Me.tsx
+++ view/authorize-view/src/layouts/components/Header/Me.tsx
@@ -1,42 +1,34 @@
-import { Avatar, Dropdown, Menu, Space } from "antd";
+import { Avatar, Dropdown, Space } from "antd";
 import { DownOutlined, UserOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
-//
+
+
 const Me = (_props: any) => {
   const navigate = useNavigate();
-
-  const menu = (
-    <Menu
-      onClick={e => {
-        let { key } = e;
-        if (key === "/logout") {
-          localStorage.removeItem("token");
-          localStorage.removeItem("jwt_token");
-          navigate("/login");
-        } else {
-          navigate(key);
-        }
-      }}
-      items={[
-        {
-          key: "/account",
-          label: "我的信息",
-        },
-        {
-          key: "/logout",
-          label: "退出",
-        },
-      ]}
-    />
-  );
+  const items = [
+    {
+      key: "account",
+      label: "我的信息",
+    },
+    {
+      key: "logout",
+      label: <a onClick={() => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("jwt_token");
+        navigate("/login");
+      }}>
+        退出
+      </a>,
+    }
+  ];
   return (
-    <Dropdown overlay={menu}>
+    <Dropdown menu={{ items }}>
       <Space style={{ margin: "0 40px 0 20px" }}>
         <Avatar icon={<UserOutlined />} />
         about me
         <DownOutlined />
       </Space>
-    </Dropdown>
+    </Dropdown >
   );
 };
 
