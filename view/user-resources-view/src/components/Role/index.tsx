6ed4e573421ec11b04ff9ebc80276d10479d7de4
yil3 <1yif@sina.com>
2022-12-02 00:05:27 +0800

    ..

--- view/user-resources-view/src/components/Role/index.tsx
+++ view/user-resources-view/src/components/Role/index.tsx
@@ -3,50 +3,47 @@
 * @Date 2022-11-28 09:40:18
 */
 
-import { Card, List } from "antd";
+import { Card } from "antd";
 import VirtualList from "rc-virtual-list";
 import { useEffect, useState } from "react";
 import { getRolePage } from '@/api/modules/role';
+import "./index.less";
 
 interface RoleItem {
   id: String;
+  name: String;
 }
 
 export default function Role() {
   const [data, setData] = useState([]);
   const [params, setParams] = useState({ page: 1, size: 10 });
+  const [loadover, setLoadover] = useState(false);
   const appendData = async () => {
     const res = await getRolePage(params);
-    if (res.success) {
+    if (res.success && !data.find((item: RoleItem) => item.id === res.data[0].id)) {
       setData(data.concat(res.data));
-      setParams({ ...params, page: params.page + 1 });
+    }
+    if (res.data.length < params.size) {
+      setLoadover(true);
     }
 
   };
   const onScroll = (e: any) => {
-    console.log(e.currentTarget.scrollTop)
-    console.log(e.currentTarget.scrollHeight)
-    console.log(e.currentTarget.clientHeight)
-    if (e.currentTarget.scrollHeight - e.currentTarget.scrollTop === e.currentTarget.clientHeight) {
-      console.log("滚动到底部了");
-      // setParams({ ...params, page: params.page + 1 });
-      // appendData();
+    if (e.currentTarget.scrollHeight - e.currentTarget.scrollTop === e.currentTarget.clientHeight && !loadover) {
+      setParams({ ...params, page: params.page + 1 });
     }
   };
-  useEffect(() => { appendData() }, []);
+  useEffect(() => { appendData() }, [params]);
   return (
-    <List>
-      <VirtualList
-        itemHeight={50}
-        itemKey="id"
-        height={200}
-        data={data}
-        onScroll={onScroll}
-      >
-        {(item: RoleItem) => (
-          <Card>{item.id}</Card>
-        )}
-      </VirtualList>
-    </List>
+    <VirtualList
+      height={600}
+      itemKey="id"
+      data={data}
+      onScroll={onScroll}
+    >
+      {(item: RoleItem) => (
+        <Card style={{width: "50px"}}>{item.name}</Card>
+      )}
+    </VirtualList>
   );
 }
